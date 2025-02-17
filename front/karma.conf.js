77765b9a593e37e5f128b4794f027fc18ac445d6
Mathilde Chaumard <mathilde.chaumard@gmail.com>
2025-02-17 14:24:43 +0100

    Code coverage

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'lcov' }
+        {type: 'json', subdir: './', file: 'coverage.json'}
       ]
     },
     reporters: ['progress', 'kjhtml'],
